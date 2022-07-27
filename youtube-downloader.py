11269ff30c4a042a98d9934efeccfba7e12526f4
etern4lslumb3r <jonasilagan173@gmail.com>
2022-07-27 15:28:09 +0800

    Update youtube-downloader.py

--- youtube-downloader.py
+++ youtube-downloader.py
@@ -58,6 +58,9 @@ class GUI:
         self.GREEN = Fore.GREEN
         self.RESET = Style.RESET_ALL
     
+    def clear():
+        return "\n"*150
+    
     def inputURL_page(self):
         global search_query, yt
         search_query = input(self.CYAN+"Insert URL or search query here: "+self.RESET)
@@ -66,10 +69,17 @@ class GUI:
             
     def askmode_page(self):
         global mode
-        print(self.CYAN+ "What would you like to convert the video to?")
+        print(self.CYAN+ "What would you like to convert the video to?\n")
         print(self.RED+"1. MP3")
         print("2. MP4"+self.RESET)
-        mode = input(self.YELLOW+"\nChoice: "+self.RESET)
+        while True:
+            try:
+                mode = int(input(self.YELLOW+"\n\nChoice: "+self.RESET))
+                if mode not in [1,2]:
+                    continue
+                break
+            except:
+                continue
         if yt.is_link:
             return self.display_information(yt.YT)
         return self.display_suggestions()
@@ -92,29 +102,30 @@ class GUI:
 
     def display_information(self, video):
         global yt, is_mp3
-        print("These are the following video details:")
+        print("\n\nThese are the following video details:\n")
         print(f"{self.YELLOW}Title:{self.RESET} {video.title}")
         print(f"{self.YELLOW}Author:{self.RESET} {video.author}")
         print(f"{self.YELLOW}Views:{self.RESET} {video.views}")
         print(f"{self.YELLOW}Length:{self.RESET} {timedelta(seconds=video.length)}")
         print(f"{self.YELLOW}Publish date:{self.RESET} {video.publish_date}")
         
-        print(f"{self.YELLOW}What would you like to do?")
+        print(f"{self.YELLOW}What would you like to do?\n")
         print(f"{self.GREEN}1. Continue to download")
         print(f"{self.RED}2. Go back{self.RESET}")
         while True:
-            choice = int(input(f"{self.YELLOW}Choice: {self.RESET}"))
+            choice = int(input(f"{self.YELLOW}\nChoice: {self.RESET}"))
 
-            if choice == 1:
+            if choice == 1:    # choice 1 = continue to download 
                 #redeclare yt downloader object with valid url
                 yt = YouTubeDownloader(search=f"https://www.youtube.com/watch?v={video.video_id}")
-                if mode == 2:
+                if mode == 2: # mode 1 == mp3 || mode 2 == mp4
                     is_mp3 = False
                     return self.choose_resolution()
-                else:
-                    print(f"{self.GREEN}Starting download for {video.format}{self.RESET}")
+                elif mode == 1:
+                    print(f"\n{self.GREEN}Starting download for {video.title}{self.RESET}\n")
                     is_mp3 = True
                     return self.download_page()
+            # if user wants to go back and user initially put a valid youtube link, return user to search input.
             elif choice == 2:
                 if yt.is_link:
                     return self.inputURL_page()
@@ -124,7 +135,7 @@ class GUI:
             continue
             
     def choose_resolution(self):
-        print(f"{self.CYAN}Available resolutions for download: ")
+        print(f"{self.CYAN}Available resolutions for download: (Please Wait)\n")
         resolutions = yt.fetch_available_resolutions()
         print(f"{self.GREEN}{resolutions}{self.RESET}")
         chosen_resolution = int(input("Choose your resolution: "))
@@ -133,65 +144,16 @@ class GUI:
         
     def download_page(self, resolution=None):
         if is_mp3:
-            input(f"{self.GREEN}MP3 file is ready to be downloaded. Press Enter to start.{self.RESET}")
+            input(f"{self.GREEN}\nMP3 file is ready to be downloaded. Press Enter to start.{self.RESET}")
             yt.download_mp3()
         elif not is_mp3:
-            input(f"{self.GREEN}MP4 file is ready to be downloaded. Press Enter to start.{self.RESET}")
+            input(f"{self.GREEN}\nMP4 file is ready to be downloaded. Press Enter to start.{self.RESET}")
             yt.download_video(resolution)
-        
         
         
 if __name__ == "__main__":
     gui = GUI()
-    gui.inputURL_page()
+    while True:
+        gui.inputURL_page()
+        gui.clear()
     
-    
-    
-    
-    
-    
-"""  while True:
-        search = input(CYAN+"\n\nInput URL or search for video: "+RESET)
-        yt = YouTubeDownloader(search=search)
-        print("Would you like to convert to MP3 or MP4?")
-        print(RED+"1. MP3")
-        print("2. MP4"+RESET)
-        while True:
-            try:
-                mode = int(input("Input: "))
-                if mode not in [1,2]:
-                    continue
-                break
-            except:
-                print(RED+"\nWrong input. Type in 1 or 2\n"+RESET)
-                continue
-            
-            
-        if not yt.is_link:
-            suggestions = yt.fetch_videos_from_search()
-            for index, video in enumerate(suggestions):
-                print(GREEN+f"{index}:{RESET} {video.title}{YELLOW} BY: {video.author}"+RESET)
-            
-            choose_from_suggestion = suggestions[int(input(GREEN+"\nChoose from suggestions list: "+RESET))]
-            yt = YouTubeDownloader(search=f"https://www.youtube.com/watch?v={choose_from_suggestion.video_id}")
-            
-            
-            
-            
-            
-            if mode == 2:
-                choose_resolution()
-                print(GREEN+"DOWNLOADING VIDEO"+RESET)
-                yt.download_video(chosen_resolution)
-            elif mode == 1:
-                print(GREEN+"DOWNLOADING MP3"+RESET)                
-                yt.download_mp3()
-        
-        elif yt.is_link:
-            if mode == 2:
-                choose_resolution()
-                print(GREEN+"DOWNLOADING VIDEO"+RESET)
-                yt.download_video(chosen_resolution)
-            elif mode == 1:
-                print(GREEN+"DOWNLOADING MP3"+RESET)
-                yt.download_mp3()"""
